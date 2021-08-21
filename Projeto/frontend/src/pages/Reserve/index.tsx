aef63d5e18e46eac12186908cffe75201ce7c377
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-08-20 22:24:10 -0300

    Ajustando CSS da página de reservas

--- Projeto/frontend/src/pages/Reserve/index.tsx
+++ Projeto/frontend/src/pages/Reserve/index.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import DatePicker from 'react-datepicker';
 
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import { useParams } from 'react-router-dom';
 import Header from '../../components/Header';
+import Input from '../../components/Input';
+import SmallButton from '../../components/SmallButton';
 
 import {
   Container,
@@ -13,13 +18,57 @@ import {
   Legend,
   InfoHotel,
   InfoReserve,
+  DateInfo,
   DateContainer,
   GeneralInfos,
+  TextInfo,
+  DescriptionApartment,
+  TypeApartment,
+  ValueTotal,
+  ButtonContainer,
+  Button,
+  Divisor,
+  IdentificationContainer,
+  IdentificationTitle,
+  IdentificationButtonContainer,
 } from './styles';
 
+import api from '../../services/api';
+
+interface HotelProps {
+  id: string;
+  latitude: number;
+  longitude: number;
+}
+
+interface ApartmentProps {
+  hotel_id: string;
+  room_type: string;
+  price: number;
+}
+
+interface ApartmentsParams {
+  apartment_id: string;
+}
+
 const Reserve: React.FC = () => {
-  const [checkinDate, setCheckinDate] = useState(new Date());
-  const [checkoutDate, setCheckoutDate] = useState(new Date());
+  const formRef = useRef<FormHandles>(null);
+
+  const [startDate, setStartDate] = useState<Date>(new Date());
+  const [endDate, setEndDate] = useState<Date>(new Date());
+  const [birthDate, setBirthDate] = useState<Date>(new Date());
+
+  const params = useParams<ApartmentsParams>();
+  console.log(params.apartment_id);
+  const [apartments, setApartments] = useState<ApartmentProps[]>([]);
+
+  useEffect(() => {
+    api.get(`apartments/`);
+  }, []);
+
+  function handleSubmit() {
+    console.log('test');
+  }
 
   return (
     <Container>
@@ -38,21 +87,59 @@ const Reserve: React.FC = () => {
               <p>E-mail: hotel@gmail.com</p>
             </InfoHotel>
             <InfoReserve>
-              <DateContainer>
-                <p>Data de check-in:</p>
-                <DatePicker
-                  selected={checkinDate}
-                  onChange={date => setCheckinDate(checkinDate)}
-                />
-                <p>Data de check-out:</p>
-                <DatePicker
-                  selected={checkoutDate}
-                  onChange={date => setCheckoutDate(checkoutDate)}
-                />
-              </DateContainer>
+              <DateInfo>
+                <DateContainer>
+                  <p>Data de check-in:</p>
+                  <DatePicker
+                    selected={startDate}
+                    onChange={(date: Date) => setStartDate(date)}
+                  />
+                </DateContainer>
+                <DateContainer>
+                  <p>Data de check-out:</p>
+                  <DatePicker
+                    selected={endDate}
+                    onChange={(date: Date) => setEndDate(date)}
+                  />
+                </DateContainer>
+              </DateInfo>
+              <TextInfo>
+                <DescriptionApartment>Apartamento 18</DescriptionApartment>
+                <TypeApartment>Tipo: Casal</TypeApartment>
+                <ValueTotal>
+                  <span>Preço:</span>
+                  <p>R$ 1.000,00</p>
+                </ValueTotal>
+              </TextInfo>
             </InfoReserve>
           </GeneralInfos>
+          <ButtonContainer>
+            <Button type="button">Editar</Button>
+          </ButtonContainer>
         </DescriptionReserve>
+        <Divisor />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <IdentificationContainer>
+            <IdentificationTitle>Identifique-se</IdentificationTitle>
+
+            <p>Nome:</p>
+            <Input name="name" type="text" placeholder="Digite o seu nome" />
+            <p>E-mail:</p>
+            <Input name="email" type="email" placeholder="Digite o seu email" />
+            <p>CPF:</p>
+            <Input name="cpf" type="text" placeholder="Digite o seu CPF" />
+            <p>Data de nascimento:</p>
+            <DatePicker
+              className="datepicker"
+              selected={birthDate}
+              onChange={(date: Date) => setBirthDate(date)}
+            />
+
+            <IdentificationButtonContainer>
+              <SmallButton type="submit">CONFIRMAR</SmallButton>
+            </IdentificationButtonContainer>
+          </IdentificationContainer>
+        </Form>
       </Content>
     </Container>
   );
--- Projeto/frontend/src/pages/Reserve/styles.ts
+++ Projeto/frontend/src/pages/Reserve/styles.ts
@@ -53,10 +53,15 @@ export const GeneralInfos = styled.div`
 `;
 
 export const InfoHotel = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+
   span {
     font-family: 'Montserrat', sans-serif;
     font-weight: 500;
     font-size: 1.5rem;
+    color: #000;
   }
 
   p {
@@ -68,11 +73,160 @@ export const InfoHotel = styled.div`
 
 export const InfoReserve = styled.div`
   display: flex;
+  flex-direction: column;
+`;
+
+export const DateInfo = styled.div`
+  display: flex;
   flex-direction: row;
+  gap: 4rem;
 `;
 
 export const DateContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+
+  p {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 300;
+    color: var(--pink);
+  }
+
+  input {
+    margin-top: 0.9rem;
+    border: 1px solid var(--dark);
+    padding: 0.875rem;
+    border-radius: 0.4rem;
+    width: 12rem;
+    height: 2.125rem;
+  }
+`;
+
+export const TextInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  margin-top: 2.625rem;
+`;
+
+export const DescriptionApartment = styled.div`
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 500;
+  font-size: 1.5rem;
+  color: #000;
+`;
+
+export const TypeApartment = styled.div`
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 300;
+  font-size: 1.125rem;
+`;
+
+export const ValueTotal = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0.2rem;
+
+  span {
+    font-weight: 300;
+    font-size: 1.125rem;
+  }
+
+  p {
+    font-weight: 500;
+    font-size: 1.25rem;
+    color: #000;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const Button = styled.button`
+  width: 10rem;
+  height: 3.625rem;
+  border: 1px solid var(--blueLight);
+  border-radius: 0.4rem;
+  background: transparent;
+
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 300;
+  font-size: 1.5rem;
+  color: var(--blueLight);
+
+  transition: background-color 0.4s;
+
+  &:hover {
+    background: var(--blueLight);
+    color: var(--light);
+    font-weight: 500;
+  }
+`;
+export const Divisor = styled.div`
+  margin-top: 1rem;
+  width: 100%;
+  height: 0.01rem;
+  background: var(--dark);
+`;
+export const IdentificationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2rem;
+
+  p {
+    margin-left: 1rem;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 300;
+    color: var(--pink);
+  }
+
+  input {
+    border: 1px solid var(--dark);
+    width: 100%;
+    height: 3rem;
+    border-radius: 0.4rem;
+    padding: 0.5rem;
+  }
+
+  .datepicker {
+    margin-left: 1rem;
+    margin-top: 1rem;
+    width: 98.5%;
+    border: 1px solid var(--dark);
+    color: var(--dark);
+
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 300;
+  }
+
+  button {
+    width: 18rem;
+    height: 3.5rem;
+
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 300;
+    font-size: 1.75rem;
+  }
+`;
+
+export const IdentificationTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 500;
+  font-size: 1.25rem;
+`;
+export const IdentificationButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  margin-top: 4rem;
 `;
